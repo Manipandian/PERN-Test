8b513e4dea56904f434922919f8e5d35c1bf09c0
Manikandan <62287301+Manipandian@users.noreply.github.com>
2020-09-02 23:18:08 +0530

    check commit

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ var db = require('knex')({
       // }
   });
 
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+  // app.use((req, res, next) => {
+  //   res.header("Access-Control-Allow-Origin", "*");
+  //   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //   next();
+  // });
 
   app.use(express.json());
 
